baa3f6b0f89fab88bd3351d3c9bc28ca70d69ae8
JoohnWiick <68707130+JoohnWiick@users.noreply.github.com>
2020-10-31 13:52:56 -0300

    Delete client.lua

--- client.lua
+++
@@ -1,9 +0,0 @@
-local screenX, screenY = guiGetScreenSize()
-local x, y = (screenX/1920), (screenY/1080)
-
--- Código para teste do github
-
-function dxDraw()
-     dxDrawRectangle(x*100, y*100, x*15, y*30)
-end
-addEventHandler('onClientRender', root, dxDraw)
