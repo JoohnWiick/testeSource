c71d5407cfba66254f9477c71ec3c5c24d1563c3
JoohnWiick <68707130+JoohnWiick@users.noreply.github.com>
2020-10-31 14:19:37 -0300

    Update client.lua

--- src/client.lua
+++ src/client.lua
@@ -2,6 +2,6 @@ local screenX, screenY = guiGetScreenSize()
 local x, y = (screenX/1920), (screenY/1080)
 
 function drawRect()
-    dxDrawRectangle(x*100, y*300, x*100, y*200)
+    dxDrawRectangle(x*500, y*300, x*100, y*200)
 end
 addEventHandler('onClientRender', root, drawRect)
